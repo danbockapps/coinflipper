edfacfcc5f30301f4c86e557b4afc3c0868e6668
Dan Bock <danbock@gmail.com>
2016-07-07 16:18:36 -0400

    Working gulp process with watch and rsync

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var pump = require('pump');
 var uglify = require('gulp-uglify');
 var ts = require('gulp-typescript');
+var shell = require('gulp-shell');
 
 gulp.task('hello', function() {
   console.log('Hello Zell');
@@ -18,11 +19,26 @@ gulp.task('compress', function(cb){
   );
 });
 
-gulp.task('default', function() {
-  return gulp.src('lib/**/*.ts')
+gulp.task('compile-typescript', function() {
+  gulp.src('lib/**/*.ts')
     .pipe(ts({
       noImplicitAny: true,
       out: 'output.js'
     }))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy-html', function() {
+  gulp.src('lib/*.html')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('rsync', shell.task('./rsync.sh'));
+
+gulp.task('default', ['compile-typescript', 'copy-html', 'rsync'], function() {
+  //console.log('default done.');
+});
+
+gulp.task('watch', function() {
+  gulp.watch('lib/*', ['default']);
 });
--- lib/coinflipper.ts
+++ lib/coinflipper.ts
@@ -10,6 +10,7 @@ class Entry {
   constructor(name: String, startingPoints: number) {
     this.startingPoints = startingPoints;
     this.name = name;
+    this.firsts = 0;
   }
 
   resetPoints() {
@@ -19,15 +20,40 @@ class Entry {
 
 const entries = [
   new Entry('Dan', 1),
-  new Entry('Hazel', 5)
+  new Entry('Hazel', 5),
+  new Entry('Ada', 6)
 ];
 
 for(let i=0; i<iter; i++) {
   for(let entry of entries) {
+    entry.resetPoints();
     for(let j=0; j<gamesRemaining; j++) {
       if(Math.random() > 0.5) {
         entry.points++;
       }
     }
+  }
+
+  // Determine who's in first place
+  firstPlace(entries).firsts++;
+}
+
+printStandings();
+
+function firstPlace(entries: Entry[]) {
+  let winningEntry = new Entry(null, null);
+  winningEntry.points = -1;
+  for(let entry of entries) {
+    // obvious issues with ties
+    if(entry.points > winningEntry.points) {
+      winningEntry = entry;
+    }
+  }
+  return winningEntry;
+}
+
+function printStandings() {
+  for(let entry of entries) {
+    console.log(entry.firsts + ' ' + entry.name);
   }
 }
